f8d7a9e2b9bec87fb9f8b757e2d834754b8468a7
Hez Ronningen <code@hez.ca>
2020-12-21 13:12:37 -0800

    Move config to runtime, enabling us to put it in config/release

--- lib/homebridge_webhook.ex
+++ lib/homebridge_webhook.ex
@@ -6,19 +6,15 @@ defmodule HomebridgeWebhook do
   @type result :: Tesla.Env.result()
   @type client :: Tesla.Client.t()
 
-  @base_url Application.compile_env(:homebridge_webhook, :base_url, "http://localhost:51828")
-  @username Application.compile_env(:homebridge_webhook, :username, "admin")
-  @password Application.compile_env(:homebridge_webhook, :password, "admin")
-
   @path "/"
 
   @spec client(list()) :: client()
   def client(additional_middleware \\ []) do
     middleware =
       [
-        {Tesla.Middleware.BaseUrl, @base_url},
+        {Tesla.Middleware.BaseUrl, base_url()},
         Tesla.Middleware.JSON,
-        {Tesla.Middleware.BasicAuth, %{username: @username, password: @password}}
+        {Tesla.Middleware.BasicAuth, %{username: username(), password: password()}}
       ] ++ additional_middleware
 
     Tesla.client(middleware)
@@ -26,4 +22,8 @@ defmodule HomebridgeWebhook do
 
   @spec get(client(), list()) :: result()
   def get(client, query_params \\ []), do: Tesla.get(client, @path, query: query_params)
+
+  defp base_url, do: Application.get_env(:homebridge_webhook, :base_url, "http://localhost:51828")
+  defp username, do: Application.get_env(:homebridge_webhook, :username, "admin")
+  defp password, do: Application.get_env(:homebridge_webhook, :password, "admin")
 end
